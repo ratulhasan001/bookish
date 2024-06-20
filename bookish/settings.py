17c7b2e8ce0b689f09ceaea9afc64b454af86cde
ratulhasan001 <ratulhasan2108@gmail.com>
2024-06-21 01:06:26 +0600

    settings updated

--- bookish/settings.py
+++ bookish/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +131,6 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 
-import environ
 env = environ.Env()
 environ.Env.read_env()
 ...
